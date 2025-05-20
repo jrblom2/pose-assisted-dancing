4d7931a3fa66cef145c8464615cf5ef17917caf8
Grayson <grasnyder2001@gmail.com>
2025-05-20 15:40:21 -0500

    Yolo detects my pose and displays it on video

--- yolodance.py
+++ yolodance.py
@@ -1,6 +1,19 @@
+import cv2
 from ultralytics import YOLO
 
-model = YOLO('yolo11n-pose.pt')
 
-results = model("https://ultralytics.com/images/bus.jpg")
+model = YOLO('yolo11n-pose.pt')
+vidObj = cv2.VideoCapture(6)
+success = True
+while success:    
+    success, image = vidObj.read()   
+    result = model(image)
+    annotated_image = result[0].plot()
+
+    cv2.imshow("", annotated_image)     
+    
+    if (cv2.waitKey(1) & 0xFF == ord("q")) or (cv2.waitKey(1)==27):
+        break
+
+cv2.destroyAllWindows()
 
